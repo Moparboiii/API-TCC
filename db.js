2508ab1854068539ec1eaf1a8ed498ac33a1ed40
Guilherme Azevedo <guilhermep51d30@gmail.com>
2023-12-05 00:26:05 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: '192.168.1.5',
   user: 'api',
   password: 'api123',
-  database: 'mercadinho',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  database: 'mercadinho'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  } else {
+    console.log('Conexão ao banco de dados estabelecida.');
+  }
 });
 
 module.exports = connection;
